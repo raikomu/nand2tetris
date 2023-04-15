879a353fbb62a272b396dbe7349c0ef4a5db6e88
Raiko Murulauk <raiko4238@gmail.com>
2023-04-15 10:11:29 +0300

    Add chapter 4 Mult solution

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  // Initialize result as 0
+  @res
+  M=0
+(LOOP)
+
+  // Stop and return result if R0 is 0
+  @R0
+  D=M
+  @STOP
+  D;JEQ
+
+  // Stop and return result if R1 is 0
+  @R1
+  D=M
+  @STOP
+  D;JEQ
+
+  // Add R0 to result
+  @R0
+  D=M
+  @res
+  M=M+D
+
+  // Stop and return result if R1-1 = 0
+  @R1
+  D=M-1
+  @STOP
+  D;JEQ
+
+  // Subtract 1 from R1 for next run
+  @R1
+  M=D
+
+  // Unconditional jump
+  @LOOP
+  0;JMP
+(STOP)
+  // Store result in R2
+  @res
+  D=M
+  @R2
+  M=D
+(END)
+  @END
+  0;JMP
