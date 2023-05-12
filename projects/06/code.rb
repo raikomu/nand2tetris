212fc8c324b65b82fe0718cc97b01af3f6205aca
Raiko Murulauk <raiko4238@gmail.com>
2023-05-12 08:53:16 +0300

    Add Code module

---
+++ projects/06/code.rb
@@ -0,0 +1,59 @@
+class Code
+  COMP_TO_BINARY = {
+    '0' => '0101010',
+    '1' => '0111111',
+    '-1' => '0111010',
+    'D' => '0001100',
+    'A' => '0110000',
+    'M' => '1110000',
+    '!D' => '0001101',
+    '!A' => '0110001',
+    '!M' => '1110001',
+    '-D' => '0001111',
+    '-A' => '0110011',
+    '-M' => '1110011',
+    'D+1' => '0011111',
+    'A+1' => '0110111',
+    'M+1' => '1110111',
+    'D-1' => '0001110',
+    'A-1' => '0110010',
+    'M-1' => '1110010',
+    'D+A' => '0000010',
+    'D+M' => '1000010',
+    'D-A' => '0010011',
+    'D-M' => '1010011',
+    'A-D' => '0000111',
+    'M-D' => '1000111',
+    'D&A' => '0000000',
+    'D&M' => '1000000',
+    'D|A' => '0010101',
+    'D|M' => '1010101'
+  }.freeze
+
+  class << self
+    def dest(symbol)
+      return '000' if symbol.nil?
+
+      code = ''
+
+      symbol.include?('A') ? code << '1' : code << '0'
+      symbol.include?('D') ? code << '1' : code << '0'
+      symbol.include?('M') ? code << '1' : code << '0'
+    end
+
+    def comp(symbol)
+      COMP_TO_BINARY.fetch(symbol)
+    end
+
+    def jump(symbol)
+      return '000' if symbol.nil?
+      return '111' if symbol == 'JMP'
+
+      code = ''
+
+      %w(L NE).any? { symbol.include?(_1) } ? code << '1' : code << '0' # '< 0' bit
+      %w(LE GE EQ).any? { symbol.include?(_1) } ? code << '1' : code <<  '0' # '= 0' bit
+      %w(G NE).any? { symbol.include?(_1) } ? code << '1' : code << '0' # '> 0' bit
+    end
+  end
+end
